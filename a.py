ab67ecd0ff8bc0108887a655b8f4ba17bf2249ae
Szabo Kristof <szabokristof6@gmail.com>
2022-01-05 09:45:40 +0100

    kesz

--- a.py
+++ a.py
@@ -1,9 +1,38 @@
+def szorzat(lista):
+    szam = 1
+    for i in lista:
+        szam *= i
+    return szam
+
+def harmadik(lista):
+    for i in lista:
+        if i > 1 and i < 10:
+            return True
+    return False
+
+def egyiklegkisebb(lista):
+    i = 0
+    szam = lista[0]
+    szam_index = 0
+    while i > 0:
+        if i < szam:
+            szam = lista[i]
+            szam_index = i
+        i += 1
+    return szam_index
+    
+
 lista = []
 
 with open("input.txt","r", encoding="utf8") as f:
     for szam in f:
         lista.append(int(szam))
     
+print(f'1. Mennyi a sorozatban található számok szorzata? {szorzat(lista)}')
+print(f'3. Írjuk ki az első 3-mal és 7-tel osztható szám indexét! {list(filter(lambda x : x%3==0 and x%7 == 0, lista))[0]}')
+print(f'5. Van-e a sorozatban olyan szám, amelyik 1 és 10 közé esik? {harmadik(lista)}')
+print(f'7. Mennyi a sorozatban található egyik legkisebb szám indexe? {egyiklegkisebb(lista)}')
+print(f'9. Hány eleme van a sorozatnak? {len(lista)}')
 
 # 3.000-D
 # Olvassuk be az alábbi fájl tartalmmát egy listába/tömbbe,
